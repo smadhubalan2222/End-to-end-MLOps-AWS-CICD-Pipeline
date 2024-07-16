6901eeaa737d4b4b36346d1c66c069421c83780b
smadhubalan2222 <smadhubalan2222@hotmail.com>
2024-07-17 02:47:47 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from mlopsProject import logger
+from mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is out custom log!")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
